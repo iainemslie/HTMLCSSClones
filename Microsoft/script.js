0e2ab09775cadd3151f6276b9042d40e83d1dcd1
Iain Emslie <iainemslie@gmail.com>
2024-01-08 15:25:05 -0500

    Added javascript for mobile navbar

--- Microsoft/script.js
+++ Microsoft/script.js
@@ -1,5 +1,67 @@
+// 860px is point where navbar stops having hamburger menu
+const navBarResizePoint = 860;
+
 window.onresize = () => {
-  if (window.innerWidth >= 1080) {
-    console.log(window.innerWidth);
+  if (window.innerWidth >= navBarResizePoint) {
+    createDesktopNavbar();
+  } else {
+    createMobileNavbar();
   }
 };
+
+function createDesktopNavbar() {
+  console.log('desktop');
+  // Insert desktop navbar
+  const navbar = document.querySelector('.nav-bar');
+  navbar.replaceChildren('Desktop');
+}
+
+function createMobileNavbar() {
+  console.log('mobile');
+  // Insert mobile navbar
+  const navbar = document.querySelector('.nav-bar');
+  let link = document.createElement('a');
+  let icon = document.createElement('i');
+
+  // Hamburger
+  icon.setAttribute('class', 'fa-solid fa-bars');
+  link.setAttribute('href', '#');
+  link.appendChild(icon);
+  navbar.replaceChildren(link);
+
+  // Search
+  link = document.createElement('a');
+  icon = document.createElement('i');
+
+  icon.setAttribute('class', 'fa-solid fa-magnifying-glass');
+  link.setAttribute('href', '#');
+  link.appendChild(icon);
+  navbar.appendChild(link);
+
+  // Navbar Logo
+  link = document.createElement('a');
+  image = document.createElement('img');
+
+  image.setAttribute('id', 'navbar-logo');
+  image.setAttribute('src', 'img/main-logo.png');
+  link.appendChild(image);
+  navbar.appendChild(link);
+
+  // Shopping Cart
+  link = document.createElement('a');
+  icon = document.createElement('i');
+
+  icon.setAttribute('class', 'fa-solid fa-cart-shopping');
+  link.setAttribute('href', '#');
+  link.appendChild(icon);
+  navbar.appendChild(link);
+
+  // User Icon
+  link = document.createElement('a');
+  icon = document.createElement('i');
+
+  icon.setAttribute('class', 'fa-regular fa-user');
+  link.setAttribute('href', '#');
+  link.appendChild(icon);
+  navbar.appendChild(link);
+}
