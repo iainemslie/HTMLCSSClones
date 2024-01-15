51be9a22b20a104771d47f2ac04b35f1405c4c6a
Iain Emslie <iainemslie@gmail.com>
2024-01-15 13:54:35 -0500

    navbar on mobile and desktop working (janky)

--- Microsoft/script.js
+++ Microsoft/script.js
@@ -1,87 +1,41 @@
 // 860px is point where navbar stops having hamburger menu
 const navBarResizePoint = 860;
 
-window.onresize = () => {
+document.addEventListener('DOMContentLoaded', createNavbar());
+window.onresize = createNavbar;
+
+function createNavbar() {
   if (window.innerWidth >= navBarResizePoint) {
     createDesktopNavbar();
   } else {
     createMobileNavbar();
   }
-};
+}
 
 function createDesktopNavbar() {
   console.log('desktop');
-  // Insert desktop navbar
-  const navbar = document.querySelector('.nav-bar');
 
-  // Navbar Logo
-  link = document.createElement('a');
-  image = document.createElement('img');
+  const navbarMobile = document.getElementById('navbar-mobile');
+  const navbarDesktop = document.getElementById('navbar-desktop');
+  const navbarMobileLinks = document.querySelectorAll('.navbar-mobile-link');
+  const navbarDesktopLinks = document.querySelectorAll('.navbar-desktop-link');
 
-  // image.setAttribute('id', 'navbar-logo');
-  // image.setAttribute('src', 'img/main-logo.png');
-  // link.appendChild(image);
-  // navbar.replaceChildren(link);
-  navbar.replaceChildren('desktop');
-
-  // Microsoft 365
-  // Teams
-  // Copilot
-  // Windows
-  // More
-
-  // ALl Microsoft
-  // Magnifying Glass
-  // Cart
-  // User Portrait
+  navbarMobile.classList.add('hidden');
+  navbarDesktop.classList.remove('hidden');
+  navbarMobileLinks.forEach((link) => link.classList.add('hidden'));
+  navbarDesktopLinks.forEach((link) => link.classList.remove('hidden'));
 }
 
 function createMobileNavbar() {
   console.log('mobile');
-  // Insert mobile navbar
-  const navbar = document.querySelector('.nav-bar');
-  let link = document.createElement('a');
-  let icon = document.createElement('i');
 
-  // Hamburger
-  icon.setAttribute('class', 'fa-solid fa-bars');
-  link.setAttribute('href', '#');
-  link.appendChild(icon);
-  navbar.replaceChildren(link);
+  const navbarMobile = document.getElementById('navbar-mobile');
+  const navbarDesktop = document.getElementById('navbar-desktop');
+  const navbarMobileLinks = document.querySelectorAll('.navbar-mobile-link');
+  const navbarDesktopLinks = document.querySelectorAll('.navbar-desktop-link');
 
-  // Search
-  link = document.createElement('a');
-  icon = document.createElement('i');
-
-  icon.setAttribute('class', 'fa-solid fa-magnifying-glass');
-  link.setAttribute('href', '#');
-  link.appendChild(icon);
-  navbar.appendChild(link);
-
-  // Navbar Logo
-  link = document.createElement('a');
-  image = document.createElement('img');
-
-  image.setAttribute('id', 'navbar-logo');
-  image.setAttribute('src', 'img/main-logo.png');
-  link.appendChild(image);
-  navbar.appendChild(link);
-
-  // Shopping Cart
-  link = document.createElement('a');
-  icon = document.createElement('i');
-
-  icon.setAttribute('class', 'fa-solid fa-cart-shopping');
-  link.setAttribute('href', '#');
-  link.appendChild(icon);
-  navbar.appendChild(link);
-
-  // User Icon
-  link = document.createElement('a');
-  icon = document.createElement('i');
-
-  icon.setAttribute('class', 'fa-regular fa-user');
-  link.setAttribute('href', '#');
-  link.appendChild(icon);
-  navbar.appendChild(link);
+  navbarDesktop.classList.add('hidden');
+  navbarMobile.classList.remove('hidden');
+  navbarMobileLinks.forEach((link) => link.classList.remove('hidden'));
+  navbarDesktopLinks.forEach((link) => link.classList.add('hidden'));
 }
